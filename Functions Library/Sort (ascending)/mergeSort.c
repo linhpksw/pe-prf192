53a38c6ece5779446f749993de1e7aa6aff0c653
taz04 <ntt4zko04@gmail.com>
2023-02-20 09:48:18 +0700

    update sort function library

---
+++ Functions Library/Sort (ascending)/mergeSort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergeSort(float a, int start, int end)
+{
+    if (start < end)
+    {
+        int mid = start + (end - start) / 2;
+    }
+}
+
+int main()
+{
+
+    system("pause");
+    return 0;
+}
--- Functions Library/Sort (ascending)/selectionSort.c
+++ Functions Library/Sort (ascending)/selectionSort.c
@@ -3,16 +3,16 @@
 
 // To make this fuction sort array in descending order, change '>' in line 11 to '<'
 // For bigger amounts of number in array, change array size in line 6 and line 21
-void ascendBubbleSort(float a[10], const int size)
+void selectionSort(float a[10], const int size)
 {
     int i, j;
-    for (i = 0; i < size - 1; i++)
-        for (j = 0; j < size - i - 1; j++)
-            if (a[j] > a[j + 1])
+    for (i = 0; i < size; i++)
+        for (j = i; j < size; j++)
+            if (a[i] > a[j])
             {
-                int mid = a[j + 1];
-                a[j + 1] = a[j];
-                a[j] = mid;
+                int mid = a[j];
+                a[j] = a[i];
+                a[i] = mid;
             }
 }
 
@@ -20,7 +20,7 @@ int main()
 {
     float a[10] = {175.62, 252.02, 293.33, 14.47, 216.77, 46.77, 107.36, 168.71, 97.16, 91.67};
     int size = sizeof(a) / sizeof(float);
-    ascendBubbleSort(a, size);
+    selectionSort(a, size);
 
     int i;
     for (i = 0; i < size; i++) // result: sorted array in ascending order
