37de8e8eb015f2203b24bbcdbbcca91b221b24b3
Trong Linh <linhpksw@gmail.com>
2023-02-22 02:04:53 +0700

    add solution for QuynhLan Q1

--- Functions Library/Number/greatestCommonDivisor.c
+++ Functions Library/Number/greatestCommonDivisor.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int GCD(int a, int b)
-{
-    while (a != 1 && b != 1)
-    {
+int GCD(int a, int b) {
+    while (a != 1 && b != 1) {
         if (a == b)
             return a;
         if (a > b)
@@ -15,9 +13,23 @@ int GCD(int a, int b)
     return a;
 }
 
-int main()
-{
-    int n = 12, m = 24; // result: 12
+int gcdSupreme(int n1, int n2) {
+    n1 = (n1 > 0) ? n1 : -n1;
+    n2 = (n2 > 0) ? n2 : -n2;
+
+    while (n1 != n2) {
+        if (n1 > n2) {
+            n1 -= n2;
+        } else {
+            n2 -= n1;
+        }
+    }
+
+    return n1;
+}
+
+int main() {
+    int n = 12, m = 24;  // result: 12
     printf("GCD = %d\n", GCD(m, n));
 
     system("pause");
