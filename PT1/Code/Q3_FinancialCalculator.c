d98774366a187b68f560b662fe81b0612b908219
taz04 <ntt4zko04@gmail.com>
2023-02-24 17:32:45 +0700

    update PT1

--- PT1/Code/Q3_FinancialCalculator.c
+++ PT1/Code/Q3_FinancialCalculator.c
@@ -5,12 +5,31 @@
 
 int main()
 {
-    system("cls");
+    // system("cls");
     // INPUT - @STUDENT:ADD YOUR CODE FOR INPUT HERE:
+    float principle, interestRate;
+    int year;
+    char type;
+    scanf("%f", &principle);
+    scanf("%f", &interestRate);
+    scanf("%d", &year);
+    scanf(" %c", &type);
 
     // OUTPUT for marking:
     printf("\nOUTPUT:\n");
     //@STUDENT: WRITE YOUR OUTPUT HERE:
+    if (type == 'f')
+    {
+        float futureValue = principle * pow(1 + interestRate, year);
+        printf("The present amount : %.2f\n", principle);
+        printf("The future value   : %.2f", futureValue);
+    }
+    if (type == 'p')
+    {
+        float presentValue = principle * pow(1 + interestRate, -year);
+        printf("The future amount  : %.2f\n", principle);
+        printf("The present value  : %.2f", presentValue);
+    }
 
     //--FIXED PART - DO NOT EDIT ANY THINGS HERE
     printf("\n");
