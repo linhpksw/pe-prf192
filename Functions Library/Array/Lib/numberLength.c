fa36b1c6efda8d63564b65db33b742400a6240d1
Trong Linh <linhpksw@gmail.com>
2023-03-13 11:02:26 +0700

    add number length

--- Functions Library/Array/Lib/numberLength.c
+++ Functions Library/Array/Lib/numberLength.c
@@ -3,27 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ELEMENT 20
-
 int numberLength(int arr[]);
 
 int main() {
-    // int arr[MAX_ELEMENT] = {-1, 5, 6, 8, 3, 8, 6, 5, 1, -7};
-    int arr[MAX_ELEMENT];
+    int arr[] = {-1, 5, 6, 8};
 
-    int size = sizeof(arr) / sizeof(int);
-
-    // printf("%d\n", sizeof(arr));
-    // printf("%d", size);
-    // int size = numberLength(arr);
-
-    // printf("%d\n", size);
+    printf("%d", numberLength(arr));
 
     return 0;
 }
 
 int numberLength(int arr[]) {
-    int i = 0, size = 0;
-
-    return size;
+    return sizeof(arr) / sizeof(int);
 }
