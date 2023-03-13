a81844d07912ca4aac220b89786ca49c86f8bbb9
taz04 <ntt4zko04@gmail.com>
2023-03-13 12:01:04 +0700

    update array lib

---
+++ Functions Library/Array/Lib/unshiftInt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// To make this function work with float/double type, change all variables type to float EXCEPT 'i' and 'sizeArr'
+// Also remember to change %d to %f or %lf (line 18)
+void pushInt(int arr[], int newArr[], int sizeArr, int num);
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    int sizeArr = sizeof(arr) / sizeof(arr[0]);
+    int newArr[sizeArr + 1];
+    pushInt(newArr, arr, sizeArr, 5);
+
+    int i;
+    for (i = 0; i < sizeArr + 1; i++)
+    {
+        printf("%d ", newArr[i]);
+    }
+
+    system("pause");
+    return 0;
+}
+
+void pushInt(int newArr[], int arr[], int sizeArr, int num)
+{
+    int i;
+    for (i = 0; i < sizeArr; i++)
+    {
+        newArr[i + 1] = arr[i];
+    }
+    newArr[0] = num;
+}
---
+++ Functions Library/Array/Lib/unshiftString.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ELEMENT 20
+#define MAX_LENGTH 500
+
+int stringLength(char arr[][MAX_LENGTH]);
+void unshiftString(char newArr[][MAX_LENGTH], char arr[][MAX_LENGTH], char str[MAX_LENGTH]);
+
+int main()
+{
+    char arr[MAX_ELEMENT][MAX_LENGTH] = {"ele1", "ele2", "ele3", "ele4"};
+    char newArr[MAX_ELEMENT][MAX_LENGTH];
+    unshiftString(newArr, arr, "ele5");
+
+    int i;
+    printf("%d\n", stringLength(newArr));
+    for (i = 0; i < stringLength(newArr); i++)
+    {
+        printf("%s ", newArr[i]);
+    }
+
+    system("pause");
+    return 0;
+}
+
+int stringLength(char arr[][MAX_LENGTH])
+{
+    int i, size = 0;
+
+    for (i = 0; i < MAX_ELEMENT; i++)
+    {
+        if (strlen(arr[i]) > 0)
+        {
+            size++;
+        }
+    }
+
+    return size;
+}
+
+void unshiftString(char newArr[][MAX_LENGTH], char arr[][MAX_LENGTH], char str[MAX_LENGTH])
+{
+    int size = stringLength(arr);
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        strcpy(newArr[i + 1], arr[i]);
+    }
+    strcpy(newArr[0], str);
+}
