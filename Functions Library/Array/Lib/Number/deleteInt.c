ef1f86e24d6f273339a235387a7470a46b74c4e2
Trong Linh <linhpksw@gmail.com>
2023-03-14 22:50:05 +0700

    problem deleteInt

--- Functions Library/Array/Lib/Number/deleteInt.c
+++ Functions Library/Array/Lib/Number/deleteInt.c
@@ -8,32 +8,44 @@
 
 void deleteInt(int arr[], int sizeArr, int start, int length);
 
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+int main() {
+    int arr[] = {1, 2, 3, 4};
     int sizeArr = sizeof(arr) / sizeof(arr[0]);
-    int length = 2; // this is the amount of elements you want to delete
+    int start = 0;   // this is the start index you want to delete
+    int length = 7;  // this is the amount of elements you want to delete
 
-    deleteInt(arr, sizeArr, 3, length); // after this, sizeArr will still keep the old value
+    deleteInt(arr, sizeArr, start, length);  // after this, sizeArr will still keep the old value
 
     int i;
-    for (i = 0; i < sizeArr - length; i++)
-    {
-        printf("%d ", arr[i]);
+
+    if (length >= sizeArr) {
+        for (i = 0; i < sizeArr; i++) {
+            printf("%d ", arr[i]);
+        }
+
+    } else {
+        for (i = 0; i < sizeArr - length; i++) {
+            printf("%d ", arr[i]);
+        }
     }
 
     return 0;
 }
 
-void deleteInt(int arr[], int sizeArr, int start, int length)
-{
-    int i;
-    for (i = start; i < start + length; i++)
-    {
-        arr[i] = arr[i + length];
-    }
-    for (i = start + length; i < sizeArr; i++)
-    {
-        arr[i] = 0;
+void deleteInt(int arr[], int sizeArr, int start, int length) {
+    // If start is out of bounds, do nothing
+    if (start < sizeArr) {
+        // If length is out of array size, set it to array size
+        if (length >= sizeArr) {
+            length = sizeArr;
+        }
+
+        int i;
+        for (i = start; i < start + length; i++) {
+            arr[i] = arr[i + length];
+        }
+        for (i = start + length; i < sizeArr; i++) {
+            arr[i] = 0;
+        }
     }
 }
